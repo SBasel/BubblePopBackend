9fb6c246f3ae7775de83853a359bc10aee679b09
Sebastian Basel <sebastian.basel@dci.education>
2023-12-12 11:24:38 +0100

    Exildraw + Routes

--- middlewares/validate/password.validate.js
+++ middlewares/validate/password.validate.js
@@ -1,7 +1,9 @@
 import { check } from "express-validator";
 
-export const passwordvalidation = check("password")
+export const passwordvalidation = check("score")
   .matches(/^(?=.*[A-Z]).{8,}$/)
   .escape()
   .trim()
   .withMessage("Das Passwort ist zu unsicher");
+
+
